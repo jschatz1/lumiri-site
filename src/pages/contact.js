50166c78c5d6fc5644297e82d50aa0e107c1c195
Michael Wells <mwells1286@gmail.com>
2020-05-29 11:59:47 -0700

    update contact info and map

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 import ContactForm from "../components/ContactForm"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import GoogleMap from "../components/GoogleMap"
+import map from "../images/map.png"
 export default () => {
   const boxStyle = {
     margin: "20px",
@@ -22,21 +22,40 @@ export default () => {
             <div className="box" style={boxStyle}>
               <div className="number-wrap">
                 <h2 className="title is-4">
+                  Email:
+                  <span className="is-size-5">
+                    <a href="mailto:support@lumiri.com"> support@lumiri.com</a>
+                  </span>{" "}
+                </h2>
+                <h2 className="title is-4">
                   Phone:
-                  <span className="is-size-5"> (240) 241-0420</span>{" "}
+                  <span className="is-size-5">
+                    {" "}
+                    <a href="tel:(240) 241-0420">(240) 241-0420</a>
+                  </span>{" "}
                 </h2>
 
                 <h2 className="title is-4">
-                  Fax:<span className="is-size-5"> (862) 298-0732</span>
+                  Fax:
+                  <span className="is-size-5">
+                    {" "}
+                    <a
+                      href="tel:(862)
+                    298-0732"
+                    >
+                      (862) 298-0732
+                    </a>
+                  </span>
                 </h2>
 
                 <h2 className="is-size-4">Address:</h2>
-                <div className="is-size-5">
-                  <p>5550 Friendship Blvd Suite 360</p>
-                  <p> Chevy Chase, MD 20815</p>
-                </div>
-
-                <GoogleMap />
+                <a target="_blank" href="https://goo.gl/maps/ZZBcm28HgATFGErp6">
+                  <div className="is-size-5">
+                    <p>5550 Friendship Blvd Suite 360</p>
+                    <p> Chevy Chase, MD 20815</p>
+                  </div>
+                  <img src={map} alt="Map of Lumiri location" />
+                </a>
               </div>
             </div>
           </div>
