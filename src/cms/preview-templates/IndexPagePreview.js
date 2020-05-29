6f5bb8774d1f0455ba0ede434e8793b2831b467c
Michael Wells <mwells1286@gmail.com>
2020-05-29 11:58:41 -0700

    add index preview page

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import React from "react"
+import PropTypes from "prop-types"
+import { IndexPageTemplate } from "../../templates/index-page"
+import Content, { HTMLContent } from "../../components/Content"
 
-const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
+const IndexPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(["data"]).toJS()
 
   if (data) {
+    console.log(widgetFor("body").props.value)
     return (
       <IndexPageTemplate
-        image={getAsset(data.image)}
         title={data.title}
         heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
+        main={data.main}
+        content={widgetFor("body")}
       />
     )
   } else {
@@ -26,7 +25,7 @@ IndexPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 }
 
 export default IndexPagePreview
