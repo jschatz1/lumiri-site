fd7194a886a2518bc19b75ce7af0b464962812f5
Michael Wells <mwells1286@gmail.com>
2020-06-02 10:03:03 -0700

    add fa icon handeling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { Link } from "gatsby"
 import React from "react"
 import logo from "../images/logo.png"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons"
 
 const NavBar = () => {
   const [isActive, setisActive] = React.useState(false)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,11 @@
 import React from "react"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
 import Footer from "./Footer"
 import NavBar from "./NavBar"
 import "./styles.scss"
+
+config.autoAddCss = false
 
 const Layout = ({ children }) => {
   return (
